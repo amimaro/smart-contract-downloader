a234cc55bccaf28aa67ef4837e37e65b10d70622
amimaro <amir.zahlan@gmail.com>
2022-01-09 11:41:26 -0300

    Componetize icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { AppForm } from "./components/AppForm";
 import { AppButton } from "./components/AppButton";
+import { DownloadIcon } from "./components/icons/DownloadIcon";
 import {
   exportContractContentsToZip,
   getContractContentList,
@@ -40,20 +41,7 @@ function App() {
 
       <AppButton onClick={downloadContract}>
         <div className="flex gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
-            />
-          </svg>{" "}
+          <DownloadIcon />
           <span>Download Contract</span>
         </div>
       </AppButton>
--- src/components/AppForm.jsx
+++ src/components/AppForm.jsx
@@ -1,5 +1,6 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { AppButton } from "./AppButton";
+import { DocumentIcon } from "./icons/DocumentIcon";
 
 export const AppForm = ({ submitForm }) => {
   return (
@@ -81,20 +82,7 @@ export const AppForm = ({ submitForm }) => {
             </div>
             <AppButton type="submit" isSubmitting={isSubmitting}>
               <div className="flex gap-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-                  />
-                </svg>{" "}
+                <DocumentIcon />
                 <span>Fetch Contract</span>
               </div>
             </AppButton>
