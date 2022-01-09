c894e7686f6938166318faa54ee9fe088542620a
amimaro <amir.zahlan@gmail.com>
2022-01-09 11:37:42 -0300

    Separete fetch and download logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,63 @@
+import { useState } from "react";
 import { AppForm } from "./components/AppForm";
+import { AppButton } from "./components/AppButton";
+import {
+  exportContractContentsToZip,
+  getContractContentList,
+  getContractSourceCode,
+} from "./lib/helpers";
 
 function App() {
+  const [contract, setContract] = useState({
+    address: "",
+    contents: [],
+  });
+  const fetchContract = async (apiKey, contractAddress) => {
+    const result = await getContractSourceCode(apiKey, contractAddress);
+    const sourceCodes = result.data.result;
+    const contractContents = getContractContentList(sourceCodes);
+    setContract({
+      address: contractAddress,
+      contents: contractContents,
+    });
+  };
+
+  const downloadContract = () => {
+    exportContractContentsToZip(contract.contents, contract.address);
+  };
+
   return (
     <div className="md:px-10 px-4 pt-8 w-full">
       <h1 className="text-2xl font-semibold tracking-widest text-center pb-4">
         Contract Downloader
       </h1>
       <div className="flex justify-center w-full">
-        <div className="border-2 md:px-8 px-2 py-4 rounded-sm lg:w-1/3 md:w-2/3 w-full">
-          <AppForm />
+        <div className="border-2 md:px-8 px-2 py-4 rounded-sm lg:w-2/5 md:w-3/5 w-full">
+          <AppForm submitForm={fetchContract} />
         </div>
       </div>
       <hr className="my-6" />
+
+      <AppButton onClick={downloadContract}>
+        <div className="flex gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+            />
+          </svg>{" "}
+          <span>Download Contract</span>
+        </div>
+      </AppButton>
+      {contract.address}
     </div>
   );
 }
--- src/components/AppForm.jsx
+++ src/components/AppForm.jsx
@@ -1,11 +1,7 @@
-import {
-  exportContractContentsToZip,
-  getContractContentList,
-  getContractSourceCode,
-} from "../lib/helpers";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import { AppButton } from "./AppButton";
 
-export const AppForm = () => {
+export const AppForm = ({ submitForm }) => {
   return (
     <Formik
       initialValues={{
@@ -28,14 +24,10 @@ export const AppForm = () => {
       }}
       onSubmit={(values, { setSubmitting }) => {
         (async () => {
-          const result = await getContractSourceCode(
-            values.apiKey,
-            values.contractAddress
-          );
-          const sourceCodes = result.data.result;
-          const contractContents = getContractContentList(sourceCodes);
-          exportContractContentsToZip(contractContents, values.contractAddress);
-          setSubmitting(false);
+          await submitForm(values.apiKey, values.contractAddress);
+          setTimeout(() => {
+            setSubmitting(false);
+          }, 400);
         })();
       }}
     >
@@ -43,14 +35,14 @@ export const AppForm = () => {
         <Form>
           <div className="flex flex-col gap-4 items-center">
             <div className="w-full flex flex-col gap-2">
-              <label className="font-semibold" htmlFor="apiKey">
+              <label className="font-semibold text-center" htmlFor="apiKey">
                 Etherscan API Key
               </label>
               <Field
                 id="apiKey"
                 type="text"
                 name="apiKey"
-                className={`border-2 w-full p-2 rounded-md ${
+                className={`border-2 w-full p-2 rounded-md text-center ${
                   errors.apiKey && touched.apiKey && errors.apiKey
                     ? "ring-2 ring-red-500"
                     : "focus:ring-2 ring-blue-500"
@@ -63,14 +55,17 @@ export const AppForm = () => {
               />
             </div>
             <div className="w-full flex flex-col gap-2">
-              <label className="font-semibold" htmlFor="contractAddress">
+              <label
+                className="font-semibold text-center"
+                htmlFor="contractAddress"
+              >
                 Contract Address
               </label>
               <Field
                 id="contractAddress"
                 type="text"
                 name="contractAddress"
-                className={`border-2 w-full p-2 rounded-md ${
+                className={`border-2 w-full p-2 rounded-md text-center ${
                   errors.contractAddress &&
                   touched.contractAddress &&
                   errors.contractAddress
@@ -84,15 +79,7 @@ export const AppForm = () => {
                 className="text-xs text-red-500 pl-2"
               />
             </div>
-            <button
-              type="submit"
-              disabled={isSubmitting}
-              className={`px-4 py-2 min-w-max text-white font-semibold tracking-wider rounded-md shadow-md active:bg-blue-800 active:shadow-none ${
-                isSubmitting
-                  ? "bg-gray-700 cursor-wait"
-                  : "bg-blue-700 cursor-pointer"
-              }`}
-            >
+            <AppButton type="submit" isSubmitting={isSubmitting}>
               <div className="flex gap-2">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -105,12 +92,12 @@ export const AppForm = () => {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth={2}
-                    d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+                    d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
                   />
                 </svg>{" "}
-                <span>Download Contract</span>
+                <span>Fetch Contract</span>
               </div>
-            </button>
+            </AppButton>
           </div>
         </Form>
       )}
