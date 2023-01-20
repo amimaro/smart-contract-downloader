598faaf326011b420952e98b18df0d3526c8f05f
amimaro <amir.zahlan@gmail.com>
2023-01-20 06:44:06 -0300

    Update fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,11 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className="font-mono">
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
 export default MyApp;
